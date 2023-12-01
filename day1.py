d800d933e81dc77809b7826d2e632c9ef7dfdb6b
Rhys Darvell <s3600111@student.rmit.edu.au>
2023-12-01 20:51:02 +1100

    part 1 finished

---
+++ day1.py
@@ -0,0 +1,24 @@
+input_file = 'input1.txt'
+
+
+with open(input_file) as input:
+    first_digit = None
+    last_digit = None
+
+    total = 0
+
+    for line in input.readlines():
+        for char in line:
+            if(char.isdigit()):
+                first_digit = char
+                break
+
+        for i in range(len(line)-1, -1, -1):
+            if(line[i].isdigit()):
+                last_digit = line[i]
+                break
+
+        value = first_digit + last_digit
+        total = total + int(value)
+
+    print(total)
