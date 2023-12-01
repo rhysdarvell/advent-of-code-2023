b02b47b19073d8c7d0440fa0e23d66330af9fca9
Rhys Darvell <s3600111@student.rmit.edu.au>
2023-12-01 20:52:27 +1100

    added comments

--- day1.py
+++ day1.py
@@ -8,11 +8,13 @@ with open(input_file) as input:
     total = 0
 
     for line in input.readlines():
+        #get first digit
         for char in line:
             if(char.isdigit()):
                 first_digit = char
                 break
 
+        #get second digit
         for i in range(len(line)-1, -1, -1):
             if(line[i].isdigit()):
                 last_digit = line[i]
